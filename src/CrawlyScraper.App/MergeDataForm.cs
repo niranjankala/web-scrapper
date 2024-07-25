9f20049d679f426bf42ab0b678303d11c4677fc2
Niranjan Singh <niranjan.kala@gmail.com>
2024-07-25 14:22:42 +0530

    Completed excel export with additional images and seo keywords

--- src/CrawlyScraper.App/MergeDataForm.cs
+++ src/CrawlyScraper.App/MergeDataForm.cs
@@ -18,6 +18,8 @@ using System.Diagnostics;
 using System.Reflection;
 using Microsoft.FSharp.Data.UnitSystems.SI.UnitNames;
 using OfficeOpenXml.Packaging.Ionic.Zlib;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Eventing.Reader;
 
 
 namespace CrawlyScraper.App
@@ -46,7 +48,7 @@ namespace CrawlyScraper.App
         private void btnBrowseFolder_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
-            {                
+            {
                 if (folderDialog.ShowDialog() == DialogResult.OK)
                 {
                     txtCategoriesFolderPath.Text = folderDialog.SelectedPath;
@@ -87,7 +89,7 @@ namespace CrawlyScraper.App
                 var categories = await Task.Run(() => ReadCategories(categoriesPath));
                 progressReporter.ReportProgress(new ProgressInfo(10, "Reading categories completed successfully."));
                 await Task.Run(() => MergeProducts(categoriesFolderPath, exportPath, categories, progressReporter));
-                progressReporter.ReportProgress(new ProgressInfo(100, "Merge completed successfully."));               
+                progressReporter.ReportProgress(new ProgressInfo(100, "Merge completed successfully."));
                 MessageBox.Show("Products have been successfully merged and saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
@@ -123,36 +125,67 @@ namespace CrawlyScraper.App
 
             return categories;
         }
-
         private List<Dictionary<string, string>> ReadChildCategoryExcel(string filePath)
         {
             var products = new List<Dictionary<string, string>>();
+            var additionalImages = new Dictionary<string, List<(string, string)>>();
 
             using (var package = new ExcelPackage(new FileInfo(filePath)))
             {
-                var worksheet = package.Workbook.Worksheets[0];
-                int rowCount = worksheet.Dimension.Rows;
-                int colCount = Math.Min(worksheet.Dimension.Columns, 20); // Get the first 20 columns
+                var worksheetProducts = package.Workbook.Worksheets[0];
+                if (package.Workbook.Worksheets.Count > 1)
+                {
+                    var worksheetAdditionalImages = package.Workbook.Worksheets[1];
+
+                    for (int row = 2; row <= worksheetAdditionalImages.Dimension.Rows; row++)
+                    {
+                        string key = worksheetAdditionalImages.Cells[row, 1].Text;
+                        string image = worksheetAdditionalImages.Cells[row, 2].Text;
+                        string order = worksheetAdditionalImages.Cells[row, 3].Text;
+                        if (additionalImages.ContainsKey(key))
+                        {
+                            additionalImages[key].Add((image, order));
+                        }
+                        else
+                        {
+                            additionalImages.Add(key, new List<(string, string)>() { (image, order) });
+                        }
+                    }
+                }
+
+                int rowCount = worksheetProducts.Dimension.Rows;
+                int colCount = Math.Min(worksheetProducts.Dimension.Columns, 20); // Get the first 20 columns
 
                 var headers = new List<string>();
                 for (int col = 1; col <= colCount; col++)
                 {
-                    headers.Add(worksheet.Cells[1, col].Text);
+                    headers.Add(worksheetProducts.Cells[1, col].Text);
                 }
 
                 for (int row = 2; row <= rowCount; row++)
                 {
                     var product = new Dictionary<string, string>();
+                    string productId = string.Empty;
                     for (int col = 1; col <= colCount; col++)
                     {
-                        string cellValue = worksheet.Cells[row, col].Text;
-                        if (col == 1 && !string.IsNullOrEmpty(cellValue))
+                        string cellValue = worksheetProducts.Cells[row, col].Text;
+                        if (col == 1 && headers[col - 1] == "product_id")
+                        {
+                            productId = cellValue;
+                        }
+                        if (col == 2 && !string.IsNullOrEmpty(cellValue))
                         {
                             cellValue = cellValue.Replace("&amp;", "&");
                         }
 
                         product[headers[col - 1]] = cellValue;
                     }
+
+                    if (!string.IsNullOrEmpty(productId) && additionalImages.ContainsKey(product["product_id"]))
+                    {
+                        product.Add("AdditionalImages", string.Join("|", additionalImages[productId].Select(r => $"{r.Item1}!{r.Item2}")));
+                    }
+
                     products.Add(product);
                 }
             }
@@ -163,8 +196,9 @@ namespace CrawlyScraper.App
         private void MergeProducts(string directoryPath, string outputPath, List<Category> categories, ProgressReporter progressReporter)
         {
             var mergedProducts = new List<Dictionary<string, string>>();
+            var additionalImages = new Dictionary<string, List<(string, string)>>();
             var parentCategories = categories.Where(c => c.ParentId == "0");
-
+            int productId = 1;
             foreach (var parentCategory in parentCategories)
             {
                 var parentDir = Path.Combine(directoryPath, parentCategory.Name);
@@ -180,18 +214,18 @@ namespace CrawlyScraper.App
                         if (!File.Exists(childFilePath))
                             continue;
                     }
-                    
-                    
+
+
 
                     var products = ReadChildCategoryExcel(childFilePath);
 
                     foreach (var product in products)
-                    {                        
-                        var existingProduct = mergedProducts.FirstOrDefault(p => p[""] == product[""] && p["Product Link"] == product["Product Link"]);
+                    {
+                        var existingProduct = mergedProducts.FirstOrDefault(p => p["Product Name"] == product["Product Name"] && p["Product Link"] == product["Product Link"]);
                         if (existingProduct != null)
                         {
                             string[] existingCategories = existingProduct["Categories"].Split(",");
-                            if (!existingCategories.Contains($"{parentCategory.CategoryId}")) 
+                            if (!existingCategories.Contains($"{parentCategory.CategoryId}"))
                             {
                                 existingProduct["Categories"] += $",{parentCategory.CategoryId}";
                             }
@@ -202,6 +236,11 @@ namespace CrawlyScraper.App
                         }
                         else
                         {
+                            if (product.ContainsKey("product_id"))
+                            {
+                                product["product_id"] = productId.ToString();
+                            }
+                            productId++;
                             product["Categories"] = $"{parentCategory.CategoryId},{category.CategoryId}";
                             mergedProducts.Add(product);
                         }
@@ -209,72 +248,112 @@ namespace CrawlyScraper.App
                     var progress = 10 + (categoryCounter / parentCategories.Count()) * 70;
                     progressReporter.ReportProgress(new ProgressInfo(progress, $"Processed Category:{category.Name}."));
                 }
-                
-
             }
 
             CreateOutputExcel(mergedProducts, outputPath);
         }
 
-		private void CreateOutputExcel(List<Dictionary<string, string>> products, string outputPath)
-		{
-			using (var package = new ExcelPackage())
-			{
-				var worksheet = package.Workbook.Worksheets.Add("Products");
+        private void CreateOutputExcel(List<Dictionary<string, string>> products, string outputPath)
+        {
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Products");
+                var worksheetAdditionalImages = package.Workbook.Worksheets.Add("AdditionalImages");
+                var worksheetProductSEOKeywords = package.Workbook.Worksheets.Add("ProductSEOKeywords");
 
-				// Define column mappings
-				Dictionary<string, string> columnMapping = new Dictionary<string, string>()
-		{
-			{ "name(en-gb)", "Product Name" }, { "categories", "Categories" },
-			{ "sku", "SKU" }, { "upc", "" },
-			{ "ean", "" }, { "jan", "" },
-			{ "isbn", "" }, { "mpn", "" },
-			{ "location", "" }, { "quantity", "" },
-			{ "model", "Model No" }, { "manufacturer", "" },
-			{ "image_name", "Product Image" }, { "shipping", "" },
-			{ "price", "Product Price" }, { "points", "" },
-			{ "date_added", "" }, { "status", "" },
-			{ "tax_class_id", "" }, { "description(en-gb)", "" },
-			{ "meta_title(en-gb)", "" }, { "meta_description(en-gb)", "" },
-			{ "meta_keywords(en-gb)", "" }, { "stock_status_id", "" },
-			{ "store_ids", "" }, { "layout", "" },
-			{ "related_ids", "" }, { "tags(en-gb)", "" },
-			{ "sort_order", "" }, { "subtract", "" },
-			{ "minimum", "" }
-		};
+                // Define column mappings
+                Dictionary<string, string> columnMapping = new Dictionary<string, string>()
+        {
+                    {"product_id","product_id" },{ "name(en-gb)", "Product Name" }, { "categories", "Categories" },
+            { "sku", "SKU" }, { "upc", "" },
+            { "ean", "" }, { "jan", "" },
+            { "isbn", "" }, { "mpn", "" },
+            { "location", "" }, { "quantity", "" },
+            { "model", "Model No" }, { "manufacturer", "" },
+            { "image_name", "Product Image" }, { "shipping", "" },
+            { "price", "Product Price" }, { "points", "" },
+            { "date_added", "" }, { "status", "" },
+            { "tax_class_id", "" }, { "description(en-gb)", "" },
+            { "meta_title(en-gb)", "" }, { "meta_description(en-gb)", "" },
+            { "meta_keywords(en-gb)", "" }, { "stock_status_id", "" },
+            { "store_ids", "" }, { "layout", "" },
+            { "related_ids", "" }, { "tags(en-gb)", "" },
+            { "sort_order", "" }, { "subtract", "" },
+            { "minimum", "" }
+        };
 
-				var headers = columnMapping.Keys.ToList();
+                var headers = columnMapping.Keys.ToList();
 
-				// Set headers in the Excel worksheet
-				for (int i = 0; i < headers.Count; i++)
-				{
-					worksheet.Cells[1, i + 1].Value = columnMapping[headers[i]];
-				}
+                // Set headers in the Excel worksheetProducts
+                for (int i = 0; i < headers.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = columnMapping[headers[i]];
+                }
 
-				// Populate data rows
-				for (int i = 0; i < products.Count; i++)
-				{
-					var product = products[i];
-					for (int j = 0; j < headers.Count; j++)
-					{
-						if (product.ContainsKey(headers[j]))
-						{
-							worksheet.Cells[i + 2, j + 1].Value = product[headers[j]];
-						}
-						else
-						{
-							worksheet.Cells[i + 2, j + 1].Value = "";
-						}
-					}
-				}
+                // Set headers in the Excel worksheetAdditionalImages
+                worksheetAdditionalImages.Cells[1, 1].Value = "product_id";
+                worksheetAdditionalImages.Cells[1, 2].Value = "image";
+                worksheetAdditionalImages.Cells[1, 3].Value = "sort_order";
 
-				// Save Excel package to file
-				package.SaveAs(new FileInfo(outputPath));
-			}
-		}
+                // Set headers in the Excel worksheetProductSEOKeywords
+                worksheetProductSEOKeywords.Cells[1, 1].Value = "product_id";
+                worksheetProductSEOKeywords.Cells[1, 2].Value = "store_id";
+                worksheetProductSEOKeywords.Cells[1, 3].Value = "keyword(en-gb)";
 
-		private void UpdateProgressBar(ProgressInfo progressInfo)
-		{
+                int imagesRowIndex = 2;
+                // Populate data rows
+                for (int i = 0; i < products.Count; i++)
+                {
+                    var product = products[i];
+                    string productId = string.Empty;
+                    string productName = string.Empty;
+                    for (int j = 0; j < headers.Count; j++)
+                    {
+                        string column = columnMapping[headers[j]];
+                        if (product.ContainsKey(column))
+                        {
+                            string value = product[columnMapping[headers[j]]];
+                            if (column == "product_id")
+                                productId = value;
+                            if (column == "Product Name")
+                                productName = value;
+                            worksheet.Cells[i + 2, j + 1].Value = value;
+                        }
+                        else
+                        {
+                            worksheet.Cells[i + 2, j + 1].Value = "";
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(productId) && !string.IsNullOrEmpty(productName))
+                    {
+                        worksheetProductSEOKeywords.Cells[i + 2, 1].Value = productId;
+                        worksheetProductSEOKeywords.Cells[i + 2, 2].Value = "0";
+                        worksheetProductSEOKeywords.Cells[i + 2, 3].Value = productName;
+                    }
+                    if (!string.IsNullOrEmpty(productId) && product.ContainsKey("AdditionalImages"))
+                    {
+                        foreach (var imgRow in product["AdditionalImages"].Split("|"))
+                        {
+                            int colIndex = 2;
+                            worksheetAdditionalImages.Cells[imagesRowIndex, 1].Value = productId;
+                            foreach (var value in imgRow.Split("!"))
+                            {
+                                worksheetAdditionalImages.Cells[imagesRowIndex, colIndex].Value = value;
+                                colIndex++;
+                            }
+                            imagesRowIndex++;
+                        }
+                    }
+                }
+
+                // Save Excel package to file
+                package.SaveAs(new FileInfo(outputPath));
+            }
+        }
+
+        private void UpdateProgressBar(ProgressInfo progressInfo)
+        {
             if (InvokeRequired)
             {
                 Invoke(new Action<ProgressInfo>(UpdateProgressBar), progressInfo);
